a6809cc1069854726cb08a9dad2a062745aaf78f
gautampal <gautampal@gmail.com>
2014-03-27 16:41:27 +1100

    Creating the H2 DB via spring and test hibernate via /pushdata url
    Change-Id: Ie8d63205b5d67ba86e6e69207eb59093e728129c

--- HelloWorld/src/main/java/com/gautam/HelloWorld.java
+++ HelloWorld/src/main/java/com/gautam/HelloWorld.java
@@ -3,9 +3,15 @@
  */
 package com.gautam;
 
+import java.util.Date;
+
+import org.hibernate.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.gautam.hibernate.DBUser;
+import com.gautam.hibernate.HibernateUtil;
 
 /**
  * @author Gautam Pal
@@ -20,6 +26,26 @@ public class HelloWorld {
 	public String printHello() {
 
 	    return "<html><body><H1>Hello World</H1></body></html>";
+	}
+	
+	@RequestMapping("pushdata")
+	@ResponseBody
+	public String testHibernate() {
+
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		 
+		session.beginTransaction();
+		DBUser user = new DBUser();
+ 
+		user.setUserId(100);
+		user.setUsername("superman");
+		user.setCreatedBy("system");
+		user.setCreatedDate(new Date());
+ 
+		session.save(user);
+		session.getTransaction().commit();
+		
+	    return "Success";
 	}
 	
 }
